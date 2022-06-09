16b305342a480cb35a06b340bb8798ad070e19d9
Tony Sathre <tony@ghostbit.org>
2022-06-08 20:27:27 -0500

    remove mayastor

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
 provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
@@ -7,7 +18,7 @@ provider "vsphere" {
 
 module "kubernetes-cluster" {
   source  = "ilpozzd/vsphere-cluster/talos"
-  version = "1.1.2"
+  version = "1.1.4"
 
   datacenter     = var.vsphere_datacenter
   datastores     = var.vsphere_datastore
@@ -36,19 +47,6 @@ module "kubernetes-cluster" {
     }
     kubelet = {
       image = "ghcr.io/siderolabs/kubelet:v${var.kubernetes_version}"
-      extraArgs = {
-        node-labels = "openebs.io/engine=mayastor"
-      }
-      extraMounts = [{
-        destination = "/var/local"
-        type        = "bind"
-        source      = "/var/local"
-        options = [
-          "rbind",
-          "rshared",
-          "rw"
-        ]
-      }]
     }
   }
 
@@ -65,21 +63,15 @@ module "kubernetes-cluster" {
   control_plane_memory   = var.control_plane_memory
 
   control_plane_disks = [{
-    label            = "root"
-    size             = 10
-    thin_provisioned = false
-    eagerly_scrub    = true
+    label = "root"
+    size  = 10
   }]
 
   control_plane_network_interfaces = [{
     name = var.vsphere_network
   }]
 
-  control_plane_machine_network_hostnames = [
-    "talos-controlplane-1",
-    "talos-controlplane-2",
-    "talos-controlplane-3"
-  ]
+  control_plane_machine_network_hostnames = var.control_plane_machine_network_hostnames
 
   control_plane_machine_cert_sans = [
     [
@@ -143,15 +135,8 @@ module "kubernetes-cluster" {
 
   worker_disks = [
     {
-      label            = "root"
-      size             = 10
-      thin_provisioned = false
-    },
-    {
-      label            = "storage"
-      size             = 40
-      thin_provisioned = false
-      eagerly_scrub    = true
+      label = "root"
+      size  = 10
     }
   ]
 
@@ -161,18 +146,7 @@ module "kubernetes-cluster" {
     }
   ]
 
-  worker_machine_extra_configuration = {
-    sysctls = {
-      "vm.nr_hugepages" = "2048"
-    }
-  }
-
-  worker_machine_network_hostnames = [
-    "talos-worker-1",
-    "talos-worker-2",
-    "talos-worker-3",
-    "talos-worker-4"
-  ]
+  worker_machine_network_hostnames = var.worker_machine_network_hostnames
 
   worker_machine_cert_sans = [
     [
@@ -232,89 +206,9 @@ module "kubernetes-cluster" {
     ]
   ]
 
-  cluster_name = "talos"
-  cluster_inline_manifests = [
-    {
-      name     = "namespace-mayastor"
-      contents = <<-EOT
-        apiVersion: v1
-        kind: Namespace
-        metadata:
-          name: mayastor
-        EOT
-    },
-    {
-      name     = "mayastor-pool-worker-1"
-      contents = <<-EOT
-      apiVersion: "openebs.io/v1alpha1"
-      kind: MayastorPool
-      metadata:
-        name: pool-worker-1
-        namespace: mayastor
-      spec:
-        node: talos-worker-1
-        disks: ["/dev/sdb"]
-      EOT
-    },
-    {
-      name     = "mayastor-pool-worker-2"
-      contents = <<-EOT
-      apiVersion: "openebs.io/v1alpha1"
-      kind: MayastorPool
-      metadata:
-        name: pool-worker-2
-        namespace: mayastor
-      spec:
-        node: talos-worker-2
-        disks: ["/dev/sdb"]
-      EOT
-    },
-    {
-      name     = "mayastor-pool-worker-3"
-      contents = <<-EOT
-      apiVersion: "openebs.io/v1alpha1"
-      kind: MayastorPool
-      metadata:
-        name: pool-worker-3
-        namespace: mayastor
-      spec:
-        node: talos-worker-3
-        disks: ["/dev/sdb"]
-      EOT
-    },
-    {
-      name     = "mayastor-pool-worker-4"
-      contents = <<-EOT
-      apiVersion: "openebs.io/v1alpha1"
-      kind: MayastorPool
-      metadata:
-        name: pool-worker-4
-        namespace: mayastor
-      spec:
-        node: talos-worker-4
-        disks: ["/dev/sdb"]
-      EOT
-    }
-  ]
-
-  cluster_extra_manifests = [
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/operator-rbac.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/mayastorpoolcrd.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/nats-deployment.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/etcd/storage/localpv.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/etcd/statefulset.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/etcd/svc.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/etcd/svc-headless.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/csi-daemonset.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/core-agents-deployment.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/rest-deployment.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/rest-service.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/csi-deployment.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor-control-plane/master/deploy/msp-deployment.yaml",
-    "https://raw.githubusercontent.com/openebs/mayastor/master/deploy/mayastor-daemonset.yaml"
-  ]
-
+  cluster_name = var.cluster_name
   kubeconfig_path       = "C:\\Users\\Tony\\.kube\\config"
   talosconfig_path      = "C:\\Users\\Tony\\.talos\\config"
   validity_period_hours = 18760
 }
+
